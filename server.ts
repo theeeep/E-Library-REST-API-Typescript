5927cd9d1c6d12fa9e006d32a4efcfc4ed9eeabe
Deepak <ideepakrabindra@gmail.com>
2024-04-12 12:32:04 +0530

    - feat: Add src/config/config.ts - chore: Update package-lock.json and package.json - chore: Update server.ts - chore: Add .env.example

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
+import { config } from "./src/config/config";
 import app from "./src/app";
 
 const startServer = () => {
-  const PORT = process.env.PORT || 3000;
+  const PORT = config.port || 3000;
 
   app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
