826a6cff9747d1d0cfccb092c2384c2c95e9903c
Deepak <ideepakrabindra@gmail.com>
2024-04-12 20:36:24 +0530

    DB connection

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import { config } from "./src/config/config";
 import app from "./src/app";
+import connectDB from "./src/config/db";
 
-const startServer = () => {
+const startServer = async () => {
+  // connect database
+  await connectDB();
+
   const PORT = config.port || 3000;
 
   app.listen(PORT, () => {
