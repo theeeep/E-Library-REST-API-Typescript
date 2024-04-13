c30597ba5321bfd0b8f4c6c2ffeaec8b8081a64e
Deepak <ideepakrabindra@gmail.com>
2024-04-13 11:06:49 +0530

    feat(user): add input validation in createUser function

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import globalErrorHandler from "./middlewares/globalErrorHandling";
 import userRouter from "./user/userRouter";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to Future" });
