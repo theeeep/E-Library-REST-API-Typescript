dfc04e2fd465e10621074d3f9573e62587e4892b
Deepak <ideepakrabindra@gmail.com>
2024-04-13 09:29:44 +0530

    - feat: Added userRouter and userController for user registration functionality - feat(user): Added userRouter and userController for user registration functionality

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandling";
+import userRouter from "./user/userRouter";
 
 const app = express();
-
-// Routes
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to Future" });
 });
+
+// Routers
+app.use("/api/users", userRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
