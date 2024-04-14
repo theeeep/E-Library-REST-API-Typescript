e15ecd782662b7a262e073eb4829979886ddfbbe
Deepak <ideepakrabindra@gmail.com>
2024-04-14 11:17:37 +0530

    feat(userController): add loginUser function for user authentication

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -49,7 +49,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       algorithm: "HS256",
     });
     // Response
-    res.json({ accesToken: token });
+    res.status(201).json({ accesToken: token });
   } catch (error) {
     return next(
       createHttpError(500, "Error Occurred while signing JWT TOKEN. !")
@@ -57,4 +57,35 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return next(createHttpError(404, "User not found."));
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return next(createHttpError(400, "username or Password incorrect!"));
+    }
+
+    // Token generation JWT
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+
+    res.json({ accessToken: token });
+  } catch (error) {
+    next(createHttpError(500, "Error Occurred while login!"));
+  }
+};
+
+export { createUser, loginUser };
