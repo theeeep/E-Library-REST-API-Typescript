7c29cc7ace8aa507de6414525cd3ac7c1ac7d9a1
Deepak <ideepakrabindra@gmail.com>
2024-04-16 03:37:25 +0530

    feat : Cors add

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import globalErrorHandler from "./middlewares/globalErrorHandling";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,12 +4,20 @@ conf();
 
 const _config = {
   port: process.env.PORT,
+
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
+
   env: process.env.NODE_ENV,
+
   jwtSecret: process.env.JWT_SECRET,
+
   cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
+
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+
   cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+
+  frontendDomain: process.env.FRONTEND_DOMAIN,
 };
 
 export const config = Object.freeze(_config);
